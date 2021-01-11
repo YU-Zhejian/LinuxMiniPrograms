3374aca1d0e0381d181cb70a0764c804c56c30d0
YuZJ <Zhejian.19@intl.zju.edu.cn>
2021-01-11 11:35:47 +0800

    Small bugs fixed

---
+++ src/exec/install_path_permissions.sh
@@ -0,0 +1,90 @@
+#!/usr/bin/env bash
+# CONFIGURE v1
+set -eu
+OLDIFS="${IFS}"
+DN="$(readlink -f "$(dirname "${0}")")"
+cd "${DN}"
+. lib/libstr
+. etc/path.sh
+#========Install PATH========
+MANCONF=true
+PACONF=false
+PYCONF=true
+INPATH="${PATH:-}"
+. lib/libpath
+unset invalid_path duplicated_path
+IFS=':'
+eachpath=(${valid_path})
+IFS=''
+for item in ${eachpath}; do
+	if [ "$(readlink -f "${item}" || true)" = "$(readlink -f "bin")" ]; then
+		infoh "PATH already configured"
+		PACONF=true
+		break
+	fi
+done
+if ! ${PACONF}; then
+	echo "export PATH=\"${DN}/bin/:\${PATH:-}\"" >> "${HOME}"/.bashrc
+	infoh "Will configure PATH...\033[32mPASSED"
+fi
+#========Install PYTHONPATH========
+if [ "${mypython}" != "ylukh" ];then
+	PYCONF=false
+	INPATH="${PYTHONPATH:-}"
+	. "${DN}"/lib/libpath
+	unset invalid_path duplicated_path
+	IFS=':'
+	eachpath=(${valid_path})
+	IFS=''
+	for item in ${eachpath}; do
+		if [ "$(readlink -f "${item}" || true)" = "$(readlink -f "libpy")" ]; then
+			infoh "PYTHONPATH already configured"
+			PYCONF=true
+			break
+		fi
+	done
+fi
+if ! ${PYCONF}; then
+	echo "export PYTHONPATH=\"${DN}/libpy/:\${PYTHONPATH:-}\"" >> "${HOME}"/.bashrc
+	infoh "Will configure PYTHONPATH...\033[32mPASSED"
+fi
+#========Install MANPATH========
+if [ -e man/man1 ]; then
+	MANCONF=false
+	INPATH="${MANPATH:-}"
+	. "${DN}"/lib/libpath
+	unset invalid_path duplicated_path
+	IFS=':'
+	eachpath=(${valid_path})
+	IFS=''
+	for item in ${eachpath}; do
+		if [ "$(readlink -f "${item}" || true)" = "$(readlink -f "man")" ]; then
+			infoh "MANPATH already configured"
+			MANCONF=true
+			break
+		fi
+	done
+fi
+if ! ${MANCONF}; then
+	echo "export MANPATH=\"${DN}/man/:\${MANPATH:-}\"" >> "${HOME}"/.bashrc
+	infoh "Will configure MANPATH...\033[32mPASSED"
+fi
+#========Install Permissions========
+function add_dir() {
+	"${myls}" -1 | while read file_name; do
+		if [ -f ${file_name} ]; then
+			"${mychmod}" -x ${file_name}
+		else
+			"${mychmod}" +x ${file_name}
+			cd ${file_name}
+			add_dir
+			cd ..
+		fi
+	done
+}
+"${mychown}" -R $(id -u) *
+"${mychmod}" -R +r+w *
+add_dir
+"${mychmod}" +x bin/* *.sh bin/exec/*.co* || true
+infoh "Modifying file permissions...\033[32mPASSED"
+IFS="${OLDIFS}"
