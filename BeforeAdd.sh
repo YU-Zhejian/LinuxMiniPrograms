f7669c84ba84e6b0d2617234961899ed0e0c85c9
YuZJ <Zhejian.19@intl.zju.edu.cn>
2021-04-07 23:12:41 +0800

    BSD small problems fixed

--- BeforeAdd.sh
+++ BeforeAdd.sh
@@ -1,11 +1,9 @@
 #!/usr/bin/env bash
 set -u +e
-DN="$(readlink -f "$(dirname "${0}")")"
-cd "${DN}"
+cd "$(readlink -f "$(dirname "${0}")")"
 if which dos2unix &>> /dev/null;then
 	/usr/bin/find . -path './.git' -prune -o -type f -print | while read fn; do dos2unix "${fn}"; done
 else
-	true
-	# TODO: bugs here.
-	# /usr/bin/find . -path './.git' -prune -o -type f -print | while read fn;do sed -i 's/\'$'\r$//g' "${fn}"; done
+	# TODO: test here.
+	/usr/bin/find . -path './.git' -prune -o -type f -print | xargs file | grep text  | cut -d: -f1 | while read fn;do echo "${fn}"; sed -i 's/\'$'\r$//g' "${fn}"; done
 fi
