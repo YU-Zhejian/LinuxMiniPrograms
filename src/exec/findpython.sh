2349edea3a0663a80ccfe47cb135336a685ec5f3
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2021-04-07 16:29:19 +0800

    Python small bugs fixed

--- src/exec/findpython.sh
+++ src/exec/findpython.sh
@@ -8,10 +8,7 @@ if [ -z "${mypython:-}" ]; then
 		ls -F -1 "${dir}" | grep '.[*@]$' | sed 's;[*@]$;;' | grep -E '^(python(3|2){0,1}(\.[[:digit:]]+)*(d|m|u){0,1}(.exe){0,1}$)' | sed "s;^;$(echo ${dir})/;" >"${tmpf}"
 		while read line; do
 			if echo "exit()" | "${line}" -v 2>&1 | grep -E '([C-Z]:\\)' > /dev/null; then
-				# TODO: test
-				# /usr/bin/python /cygdrive/d/Software/CygWin64/bin/python /cygdrive/c/Users/admin/AppData/Local/Microsoft/WindowsApps/python.exe
-				# Is Windows Python
-				echo "Windows Python found in ${line}"
+				infoh "Windows Python found in ${line}"
 				continue
 			fi
 			echo "Python found in ${line}"
