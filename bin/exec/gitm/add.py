908630082007023900d2a3cf444360802fe1ca7d
YuZJ <Zhejian.19@intl.zju.edu.cn>
2021-04-13 15:16:56 +0800

    gitm.py purposed

--- bin/exec/gitm/add.py
+++ bin/exec/gitm/add.py
@@ -4,14 +4,51 @@ gitm_add in Python
 '''
 import os
 import sys
+import uuid
 from linuxminipy.libisopt import isopt
+from linuxminipy.libstr import is_url, warnh, infoh, errh
+import libgitm
 
 VERSION=1.0
-
+errh("Too much bugs")
 def main():
-    os.chdir(os.environ('GITM_HOME'))
-    print(os.curdir)
-
+    STDS=[]
+    for sysarg in sys.argv[1:]:
+        if isopt(sysarg):
+            if sysarg in ('-h', '--help'):
+                os.system('yldoc git-mirror')
+                sys.exit(0)
+            elif sysarg in ('-v', '--version'):
+                print(str(VERSION) + ' in Python')
+                sys.exit(0)
+        else:
+            STDS.append(sysarg)
+    # TODO: lock
+    my_uuid_table=libgitm.UuidTable()
+    my_gitm_log=libgitm.GitmLog()
+    for my_url in STDS:
+        my_url.replace('file://', '')
+        if not is_url(my_url):
+            warnh('Skipping bad URL ' + my_url)
+            continue
+        if my_uuid_table.findurl(my_url):
+            warnh('Skipping existing URL ' + my_url)
+        my_uuid=uuid.uuid4().__str__()
+        while True:
+            if my_uuid_table.finduuid(my_uuid):
+                my_uuid=uuid.uuid4()
+            else:
+                break
+        infoh(my_url + ' -> ' + my_uuid)
+        if os.system('git clone --mirror --no-hardlinks --verbose --progress \'' + my_url + '\' \'' + my_uuid + '\''):
+            my_uuid_table.add(my_url, my_uuid)
+            pass
+            my_gitm_log.add('t'.join(['ADD','SUCCESS',my_url,my_uuid]))
+        else:
+            warnh(my_url + ' corrupted. Will be skipped"')
+            os.remove(my_uuid)
+            os.remove("./logs/"+my_uuid)
+            my_gitm_log.add('t'.join(['ADD','FAILED',my_url,my_uuid]))
 
 if __name__ == '__main__':
     main()
