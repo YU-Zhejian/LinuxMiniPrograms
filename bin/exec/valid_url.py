cd94ce9387730f9af34622a6e545236fb648cc3f
YU Zhejian <Zhejian.19@intl.zju.edu.cn>
2021-04-15 09:55:36 +0800

    url moved to libstr

--- bin/exec/valid_url.py
+++ bin/exec/valid_url.py
@@ -2,18 +2,12 @@
 '''
 To test whether a URL is valid.
 '''
-# VERSION=1.1
-import os
-import re
+VERSION=1.1
+from linuxminipy.libstr import is_url
 import sys
 
-sshstr = re.compile(r"ssh://([^@\s]+@){0,1}([^@:\s]+)(:[0-9]+){0,1}(/[^@:]*)")
-httpstr = re.compile(r"http(s){0,1}://([^@:\s]+)(:[0-9]+){0,1}(/[^@:]*)")
-gitstr = re.compile(r"git://([^@:\s]+)(:[0-9]+){0,1}(/[^@:]*)")
-scpstr = re.compile(r"([^@\s]+@){0,1}([^@:\s]+):(/[^@:]*)")
 def main():
-    if sshstr.match(sys.argv[1]) or gitstr.match(sys.argv[1]) or \
-            scpstr.match(sys.argv[1]) or os.path.isdir(sys.argv[1]):
+    if is_url(sys.argv[1]):
         sys.exit(0)
     else:
         sys.exit(1)
