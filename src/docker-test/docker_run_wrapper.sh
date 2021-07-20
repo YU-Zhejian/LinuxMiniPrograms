eee0bfca770582fa2c862f43a41c7535e91cec93
yuzj <theafamily@126.com>
2021-07-20 13:55:13 +0800

    Small problems found on Docker fixed

---
+++ src/docker-test/docker_run_wrapper.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# A script to handle Docker installation issues.
+# Usage: docker_wrapper.sh [FILE].Dockerfile
+# shellcheck disable=SC2034
+VERSION=1.0
+set -ue
+builtin cd "$(readlink -f "$(dirname "${0}")")" || builtin exit 1
+NAME="$(basename -s '.Dockerfile' "${1}")"
+TARGET_VERSION="$(cat "${NAME}.Dockerfile" | grep '^LABEL version' | cut -f 2 -d '=' | sed 's;";;g')"
+. ../../shlib/libinclude.sh
+__include libdo
+mkdir -p log/
+LIBDO_LOG_MODE=4
+LIBDO_LOG="log/${NAME}_$(date +%Y-%m-%d_%H-%M-%S).log"
+
+__DO docker build . --file "${1}" --tag "lmp_test_${NAME}:${TARGET_VERSION}"
+__DO docker run --rm "lmp_test_${NAME}:${TARGET_VERSION}"
