6124ddf2069c78740ac78ed0b0a150c03b78f3bd
yuzj <theafamily@126.com>
2021-07-20 19:54:49 +0800

    Usable docker test added

--- src/docker-test/bin/docker_run_wrapper.sh
+++ src/docker-test/bin/docker_run_wrapper.sh
@@ -4,7 +4,7 @@
 # shellcheck disable=SC2034
 VERSION=1.0
 set -eu
-builtin cd "$(readlink -f "$(dirname "${0}")")" || builtin exit 1
+builtin cd "$(readlink -f "$(dirname "${0}")/../")" || builtin exit 1
 NAME="$(basename -s '.Dockerfile' "${1}")"
 TARGET_VERSION="$(cat "out/${NAME}.Dockerfile" | grep '^LABEL version' | cut -f 2 -d '=' | sed 's;";;g')"
 . ../../shlib/libinclude.sh
@@ -13,5 +13,12 @@ mkdir -p log/
 LIBDO_LOG_MODE=4
 LIBDO_LOG="log/${NAME}_$(date +%Y-%m-%d_%H-%M-%S).log"
 
-__DO docker build . --file "${1}" --tag "lmp_test_${NAME}:${TARGET_VERSION}"
-__DO docker run --rm "lmp_test_${NAME}:${TARGET_VERSION}"
+__DO docker build . --file "${1}" --tag "lmp_test_${NAME}:${TARGET_VERSION}" --build-arg tarname="lmp_test_${NAME}"
+docker ps -a -f name="lmp_test_${NAME}" | grep "lmp_test_${NAME}" &>> /dev/null || docker rm --force "lmp_test_${NAME}"
+__DO docker run --name "lmp_test_${NAME}" "lmp_test_${NAME}:${TARGET_VERSION}"
+__DO docker cp "lmp_test_${NAME}:/lmp_test_${NAME}.tar" .
+__DO autounzip --remove "lmp_test_${NAME}.tar"
+__DO docker rm "lmp_test_${NAME}"
+
+# debian_10_minimal-1.0.tar
+# LMP-debian-9
